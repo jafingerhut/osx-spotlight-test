431fee920a833f7070f10dd02f1c98df0c946368
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2018-06-18 23:56:18 -0700

    Add command line option to test-sequence1.py to select which file to test

--- bin/test-sequence1.py
+++ bin/test-sequence1.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python
 
 from __future__ import print_function
+import argparse
 import json
 import os
 import subprocess
@@ -8,6 +9,39 @@ import sys
 import time
 
 import mdimporters
+
+
+pause_duration_sec = 10
+apple_ms_mdimporter = '/Library/Spotlight/Microsoft Office.mdimporter'
+dir_name = "osx-spotlight-test-files"
+github_repo = "https://github.com/jafingerhut/" + dir_name
+
+
+parser = argparse.ArgumentParser(description="""
+Try a sequence of 'git clone', mdimport, and mdfind commands on a
+selected file, to see how that sequence effects the search results.""")
+parser.add_argument('-f', '--file', dest='file',
+                    choices=['word-2016-mac-docx',
+                             'word-2016-mac-doc'],
+                    help="""Specify which file to use in the test.""")
+args = parser.parse_known_args()[0]
+
+# Note: The search term is written in this code as multiple shorter
+# strings that are then concatenated together, so that searching for
+# the term will not find the file containing this program.
+
+if args.file is None:
+    print("Must specify -f / --file option")
+    sys.exit(1)
+
+if args.file == 'word-2016-mac-docx':
+    filename = "MS-Word-for-Mac-version-16.9-Word-Document-docx.docx"
+    search_term = "vorkon" + "inkawu"
+elif args.file == 'word-2016-mac-doc':
+    filename = "MS-Word-for-Mac-version-16.9-Word-97-2004-Document-doc.doc"
+    search_term = "lowhux" + "owaachii"
+
+filename = os.path.join(dir_name, filename)
 
 
 def delete_dir(dir_name):
@@ -31,8 +65,9 @@ def do_search(search_term, filename, pause_duration_sec, msg,
     record_mdls_out(mdls_output_filename, filename)
     found_filenames = mdimporters.mdfind_search_results(search_term)
     filtered_filenames = [x for x in found_filenames if filename in x]
-    print("%d sec after %s, mdfind '%s' found %d match(es)"
-          "" % (pause_duration_sec, msg, search_term, len(filtered_filenames)))
+    print("%d sec after %s, mdfind '%s' + '%s' found %d match(es)"
+          "" % (pause_duration_sec, msg, search_term[0:6], search_term[6:],
+                len(filtered_filenames)))
     return filtered_filenames
 
 
@@ -51,25 +86,13 @@ def mdimport(msg, filename, mdimporter_choice):
 
 
 
-pause_duration_sec = 10
-apple_ms_mdimporter = '/Library/Spotlight/Microsoft Office.mdimporter'
-dir_name = "osx-spotlight-test-files"
-github_repo = "https://github.com/jafingerhut/" + dir_name
-
-filename = dir_name + "/MS-Word-for-Mac-version-16.9-Word-Document-docx.docx"
-# Note: The search term is stored separately as 'search_term_part1'
-# and 'search_term_part2' so that searching for the term will not find
-# the file containing this program.
-search_term_part1 = "vorkon"
-search_term_part2 = "inkawu"
-search_term = search_term_part1 + search_term_part2
 
 output_dir_name = "test-sequence1-out"
 delete_dir(output_dir_name)
 os.mkdir(output_dir_name)
 
 
-for pass_num in [1, 2, 3, 4, 5]:
+for pass_num in [1, 2, 3, 4, 5, 6]:
     if pass_num == 1:
         mdimporter_sequence = [None, apple_ms_mdimporter, None]
     elif pass_num == 2:
@@ -85,6 +108,8 @@ for pass_num in [1, 2, 3, 4, 5]:
         mdimporter_sequence = [apple_ms_mdimporter, None,
                                apple_ms_mdimporter, None,
                                apple_ms_mdimporter, None]
+    elif pass_num == 6:
+        mdimporter_sequence = [None, None, None, None, None, None]
 
     print("")
     print("----------------------------------------")
@@ -96,8 +121,8 @@ for pass_num in [1, 2, 3, 4, 5]:
     # bad enough that the command above gives a non-0 exit status, it will
     # abort this program.
     do_search(search_term, filename, pause_duration_sec, "'git clone'",
-              output_dir_name +
-              ("/pass-%d-mdls-after-git-clone.txt" % (pass_num)))
+              os.path.join(output_dir_name,
+                           ("pass-%d-mdls-after-git-clone.txt" % (pass_num))))
 
     i = 0
     for m in mdimporter_sequence:
@@ -105,6 +130,6 @@ for pass_num in [1, 2, 3, 4, 5]:
         mdimport("mdimport #%d complete:" % (i), filename, m)
         do_search(search_term, filename, pause_duration_sec,
                   "mdimport #%d" % (i),
-                  output_dir_name +
-                  ("/pass-%d-mdls-after-mdimport-%d.txt"
-                   "" % (pass_num, i)))
+                  os.path.join(output_dir_name,
+                               ("pass-%d-mdls-after-mdimport-%d.txt"
+                                "" % (pass_num, i))))
